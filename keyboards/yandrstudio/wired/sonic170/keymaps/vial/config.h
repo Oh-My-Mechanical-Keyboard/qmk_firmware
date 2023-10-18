decf9f10bb8309198aed300b60e66ac7b0e8091c
jiaxin96 <572673807@qq.com>
2023-10-18 19:34:12 +0800

    up sonic170 vial

---
+++ keyboards/yandrstudio/wired/sonic170/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Y&R-Biu (@jiaxin96)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAB, 0x69, 0x5E, 0x6F, 0x94, 0x9D, 0xFC, 0x63}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
